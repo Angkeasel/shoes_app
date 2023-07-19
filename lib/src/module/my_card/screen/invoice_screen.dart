20a2b1339b3225c628fc9891e84dc0bce73eefe4
Tinut-Chan <tinutchan@gmail.com>
2023-07-19 17:10:56 +0700

    invoice update

--- lib/src/module/my_card/screen/invoice_screen.dart
+++ lib/src/module/my_card/screen/invoice_screen.dart
@@ -1,5 +1,6 @@
 import 'package:allpay/src/module/my_card/widgets/customs_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
 
 class InvoiceScreen extends StatelessWidget {
@@ -15,146 +16,176 @@ class InvoiceScreen extends StatelessWidget {
             child: const Icon(Icons.arrow_back_ios)),
         title: const Text("Invoice card"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-              decoration: BoxDecoration(color: Colors.grey.shade300),
-              child: Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                decoration: BoxDecoration(color: Colors.grey.shade300),
+                child: Row(
+                  children: [
+                    Text(
+                      "Reference No :",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .copyWith(color: Colors.black),
+                    ),
+                    Text(
+                      " #000001",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Reference No :",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyLarge!
-                        .copyWith(color: Colors.black),
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      "Name",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
                   ),
-                  Text(
-                    " #000001",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.w600),
+                  Expanded(
+                    child: Text(
+                      "sized",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Qty",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Price",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Amount",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    "Name",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.w600),
+              const SizedBox(
+                height: 5,
+              ),
+              const Divider(
+                color: Colors.black,
+                thickness: 2,
+              ),
+              const CustomMenu(
+                name: 'Air Shoes',
+                sized: '37',
+                qty: 1,
+                price: 34,
+                amount: 34,
+              ),
+              const Divider(
+                color: Colors.black,
+                thickness: 2,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Subtotal : ",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Discount : ",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Total : ",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
+              ),
+              const Divider(color: Colors.black),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "សូមអរគុណសម្រាប់ការអញ្ជើញមក",
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: Colors.black, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "Thank You! for ordering",
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: Colors.black, fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      Text(
+                        "Wifi Password : aklo9999",
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: Colors.black, fontWeight: FontWeight.w600),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: Text(
-                    "sized",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                width: context.width,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  label: Text(
+                    'Print',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(fontWeight: FontWeight.w700),
+                  ),
+                  icon: const Icon(
+                    Icons.print,
+                    color: Colors.white,
                   ),
                 ),
-                Expanded(
-                  child: Text(
-                    "Qty",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.w600),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "Price",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.w600),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "Amount",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const Divider(
-              color: Colors.black,
-              thickness: 2,
-            ),
-            const CustomMenu(
-              name: 'Air Shoes',
-              sized: '37',
-              qty: 1,
-              price: 34,
-              amount: 34,
-            ),
-            const Divider(
-              color: Colors.black,
-              thickness: 2,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Subtotal : ",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Discount : ",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Total : ",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              "VAT : ",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Grand Total : ",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
